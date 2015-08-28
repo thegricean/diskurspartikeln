e6893d5822cc6644070f4cf0a5d8c8b95cacf422
thegricean <judithdegen@gmail.com>
2015-08-28 09:41:43 -0700

    added comprehension results

---
+++ experiments/2_dp_comprehension_listenerbelief/results/rscripts/dp_listener.R
@@ -0,0 +1,68 @@
+theme_set(theme_bw(18))
+setwd("/Users/titlis/cogsci/projects/stanford/projects/diskurspartikeln/experiments/2_dp_comprehension_listenerbelief/results/")
+
+source("rscripts/helpers.r")
+
+load("data/r.RData")
+r = read.table("data/results.txt",quote="", sep="\t", header=T)
+head(r)
+nrow(r)
+names(r)
+summary(r)
+r$trial = r$slide_number_in_experiment - 2
+r = r[,c("subject", "rt", "language","age","gender","trial","enjoyment","asses","comments","time_in_minutes","response","response_type","item","sentence","item_type")]
+head(r)
+
+lb = droplevels(subset(r, r$response_type=="listenerbelief"))
+r = droplevels(subset(r, r$response_type=="evidence"))
+nrow(lb)
+nrow(r)
+row.names(lb) = paste(lb$subject,lb$trial)
+r$listenerbelief = as.numeric(as.character(lb[paste(r$subject,r$trial),]$response))
+summary(r)
+
+# add directness ratings
+directness = read.csv("../../4_dp_priors_evidencestrength/results/data/evidence_priors.txt")
+str(directness)
+head(directness)
+row.names(directness) = paste(directness$item,directness$evidence_id)
+r$Directness = directness[paste(r$item,r$response),]$mean
+r$EvidenceTypeCategorical = directness[paste(r$item,r$response),]$evidence_type
+r$item_english = directness[paste(r$item,r$response),]$item_english
+
+summary(r)
+table(r$item,r$response)
+save(r,file="data/r.RData")
+
+
+##################
+
+ggplot(aes(x=gender), data=r) +
+  geom_histogram()
+
+ggplot(aes(x=rt), data=r) +
+  geom_histogram() +
+  scale_x_continuous(limits=c(0,50000))
+
+ggplot(aes(x=age), data=r) +
+  geom_histogram()
+
+ggplot(aes(x=age,fill=gender), data=r) +
+  geom_histogram()
+
+ggplot(aes(x=enjoyment), data=r) +
+  geom_histogram()
+
+ggplot(aes(x=asses), data=r) +
+  geom_histogram()
+
+ggplot(aes(x=time_in_minutes), data=r) +
+  geom_histogram()
+
+ggplot(aes(x=age,y=time_in_minutes,color=gender), data=unique(r[,c("subject","age","time_in_minutes","gender")])) +
+  geom_point() +
+  geom_smooth()
+
+unique(r$comments)
+
+####################
--- experiments/2_dp_comprehension_listenerbelief/results/rscripts/plots.R
+++ experiments/2_dp_comprehension_listenerbelief/results/rscripts/plots.R
@@ -1,232 +1,112 @@
-library(ggplot2)
 theme_set(theme_bw(18))
-setwd("~/cogsci/projects/stanford/projects/modals/modals/experiments/72_modals_comprehension_evidence_room/results/")
+setwd("/Users/titlis/cogsci/projects/stanford/projects/diskurspartikeln/experiments/2_dp_comprehension_listenerbelief/results/")
+
 source("rscripts/helpers.r")
 load("data/r.RData")
+
 summary(r)
 r.lbelief = r
+r.lbelief$response_evidence = r.lbelief$response
 r.lbelief$Experiment = "listener"
-baremust.l = r.lbelief[r.lbelief$item_type %in% c("bare","must","might"),c("Directness","item_type","item")]
+baremust.l = r.lbelief
+#baremust.l = r.lbelief[r.lbelief$item_type %in% c("bare","must","might"),c("Directness","item_type","item")]
+r.lbelief$response = r.lbelief$listenerbelief
+r.lbelief$listenerbelief = NULL
 nrow(baremust.l)
 head(baremust.l)
 
-load("~/cogsci/projects/stanford/projects/modals/modals/experiments/80_modals_comprehension_speakerbelief/results/data/r.RData")
+load("/Users/titlis/cogsci/projects/stanford/projects/diskurspartikeln/experiments/1_dp_comprehension_speakerbelief/results/data/r.RData")
 r.spbelief = r
 r.spbelief$Experiment = "speaker"
-baremust.s = r.spbelief[r.spbelief$item_type %in% c("bare","must","might"),c("Directness","item_type","item")]
+baremust.s = r.spbelief
+#baremust.s = r.spbelief[r.spbelief$item_type %in% c("bare","must","might"),c("Directness","item_type","item")]
 nrow(baremust.s)
 head(baremust.s)
-r.spbelief$response = r.spbelief$Response
-r.spbelief$Response = NULL
+r.spbelief$response_evidence = r.spbelief$response
+r.spbelief$response = r.spbelief$listenerbelief
+r.spbelief$listenerbelief = NULL
   
 r.combined = merge(r.lbelief,r.spbelief,all=T)
-r.combined$assignmentid = NULL
 head(r.combined)
+summary(r.combined)
 write.table(r.combined,file="data/combined.comprehension.experiments.txt",row.names=F,col.names=T,quote=F,sep="\t")
-write.table(r.combined,file="../../../writing/2014/cuny_2015/poster/data/combined.comprehension.experiments.txt",row.names=F,col.names=T,quote=F,sep="\t")
 
 
-baremust = droplevels(rbind(baremust.s,baremust.l))
-baremust$Experiment = as.factor(as.character(baremust$Experiment))
-
-un = unique(baremust[,c("Directness","item")])
-nrow(un)
-un$StrengthBinNumObs = cut2(un$Directness,g=3)
-un$StrengthBinSize = cut(un$Directness,breaks=3)
-row.names(un) = paste(un$Directness, un$item)
-summary(un)
-baremust$StrengthBinNumObs = un[paste(baremust$Directness, baremust$item),]$StrengthBinNumObs
-baremust$StrengthBinSize = un[paste(baremust$Directness, baremust$item),]$StrengthBinSize
-
-summary(baremust)
-
-tfreq = as.data.frame(table(baremust$StrengthBinNumObs,baremust$item_type))
-t = as.data.frame(prop.table(table(baremust$StrengthBinNumObs,baremust$item_type),mar=2))
-head(t)
-head(tfreq)
-colnames(t) = c("StrengthBin","Modal","Proportion")
-colnames(tfreq) = c("StrengthBin","Modal","Freq")
-t$Freq = tfreq$Freq
-dodge = position_dodge(.9) 
-
-t$Utterance = factor(x=t$Modal,levels=c("bare","must","might"))
-t$Strength = factor(x=t$StrengthBin,levels=rev(levels(t$StrengthBin)))
-
-ggplot(t, aes(x=Utterance,y=Proportion,fill=Strength)) +
-  geom_bar(stat="identity",position=dodge,color="black") +
-  geom_text(aes(label=Freq,y=0.02),position=dodge)
-ggsave("graphs/strength_dist_cuny_strengthbinnumobs.pdf",width=6)
-
-
-tfreq = as.data.frame(table(baremust$StrengthBinSize,baremust$item_type))
-t = as.data.frame(prop.table(table(baremust$StrengthBinSize,baremust$item_type),mar=2))
-head(t)
-head(tfreq)
-colnames(t) = c("StrengthBin","Modal","Proportion")
-colnames(tfreq) = c("StrengthBin","Modal","Freq")
-t$Freq = tfreq$Freq
-dodge = position_dodge(.9) 
-
-t$Utterance = factor(x=t$Modal,levels=c("bare","must","might"))
-t$Strength = factor(x=t$StrengthBin,levels=rev(levels(t$StrengthBin)))
-t$EvidenceBin = rep(c("weak","medium","strong"),3)
-# for justine:
-write.table(t[,c("Utterance","EvidenceBin","Proportion")],file="data/evidence_given_utterance.txt",quote=F,row.names=F,col.names=F,sep=",")
-ggplot(t, aes(x=Utterance,y=Proportion,fill=Strength)) +
-  geom_bar(stat="identity",position=dodge,color="black") +
-  geom_text(aes(label=Freq,y=0.02),position=dodge)
-ggsave("graphs/strength_dist_cuny_strengthbinsize.pdf",width=6)
-
-agr = aggregate(Directness ~ item_type, FUN=mean, data=baremust)
-agr$CILow = aggregate(Directness ~ item_type, FUN=ci.low, data=baremust)$Directness
-agr$CIHigh = aggregate(Directness ~ item_type, FUN=ci.high, data=baremust)$Directness
-agr$YMin = agr$Directness - agr$CILow
-agr$YMax = agr$Directness + agr$CIHigh
-
-ggplot(agr, aes(x=item_type, y=Directness)) +
-  geom_bar(stat="identity",fill="gray80",color="black") +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25)
-ggsave("graphs/mean_strength_cuny.pdf",width=5)
-
-
-agr = aggregate(response ~ item_type,data=r,FUN=mean)
-agr$SD = aggregate(response ~ item_type,data=r,FUN=sd)$response
-agr
-agr$CILow = aggregate(response ~ item_type,data=r, FUN=ci.low)$response
-agr$CIHigh = aggregate(response ~ item_type,data=r,FUN=ci.high)$response
-agr$YMin = agr$response - agr$CILow
-agr$YMax = agr$response + agr$CIHigh
-
-agr$Modal = factor(x=as.character(agr$item_type),levels=c("bare","must","probably","might"))
-ggplot(agr, aes(x=Modal,y=response)) +
-  geom_bar(stat="identity",fill="gray80",color="black") +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
-  xlab("Utterance") +
-  ylab("Probability")
-ggsave("graphs/means_bars.pdf",width=5.5)
-
-ggplot(agr, aes(x=Modal,y=response)) +
-  geom_point() +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25)
-ggsave("graphs/means.pdf")
-
-
-agr = aggregate(response ~ item_type + item,data=r,FUN=mean)
-agr$CILow = aggregate(response ~ item_type+ item,data=r, FUN=ci.low)$response
-agr$CIHigh = aggregate(response ~ item_type+ item,data=r,FUN=ci.high)$response
-agr$YMin = agr$response - agr$CILow
-agr$YMax = agr$response + agr$CIHigh
-
-agr$Modal = factor(x=as.character(agr$item_type),levels=c("bare","must","probably","might"))
-ggplot(agr, aes(x=Modal,y=response)) +
-  geom_point() +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
-  facet_wrap(~item) +
-  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
-ggsave("graphs/means_byitem.pdf")
-
-
-# histograms of evidence type
-t = as.data.frame(prop.table(table(r$item_type,r$evidence),mar=1))
-head(t)
-colnames(t) = c("Modal","EvidenceType","Proportion")
-t$Condition = factor(x=as.character(t$Modal),levels=c("bare","must","probably","might"))
-
-ggplot(t, aes(x=Condition,y=Proportion,fill=EvidenceType)) +
-  geom_bar(stat="identity")
-ggsave("graphs/evidence_dist.pdf")
-
-# histograms of evidence type by item
-t = as.data.frame(prop.table(table(r$item,r$item_type,r$evidence),mar=c(1,2)))
-head(t)
-colnames(t) = c("Item","Modal","EvidenceType","Proportion")
-t[t$Item == "coffee" & t$Modal == "must",]
-t$Condition = factor(x=as.character(t$Modal),levels=c("bare","must","probably","might"))
-
-ggplot(t, aes(x=Condition,y=Proportion,fill=EvidenceType)) +
-  geom_bar(stat="identity") +
-  facet_wrap(~Item) +
-  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
-ggsave("graphs/evidence_dist_byitem.pdf")
-
-
-t = as.data.frame(prop.table(table(r$item,r$item_type,r$evidence),mar=c(1,2)))
-head(t)
-colnames(t) = c("Item","Modal","Evidence","Proportion")
-t$Directness = directness[paste(t$Item,t$Evidence),]$prob
-head(t)
-
-ggplot(t, aes(x=Directness,y=Proportion)) +
-  geom_point() +
-  geom_smooth() +
-  facet_wrap(~Modal)
-ggsave("graphs/directness.pdf")
-
-t = as.data.frame(prop.table(table(r$item,r$item_type,r$EvidenceDirectnessCategorical),mar=c(1,2)))
-t
-colnames(t) = c("Item","Modal","EvidenceDirectness","Proportion")
-ggplot(t, aes(x=EvidenceDirectness,y=Proportion)) +
-  geom_bar(stat="identity") +
-  facet_grid(Item~Modal)
-ggsave("graphs/directness_categorical.pdf")
-
-agr = aggregate(Proportion~Modal+EvidenceDirectness,FUN=mean,data=t)
-agr$CILow = aggregate(Proportion~Modal+EvidenceDirectness,FUN=ci.low,data=t)$Proportion
-agr$CIHigh = aggregate(Proportion~Modal+EvidenceDirectness,FUN=ci.high,data=t)$Proportion
-agr$YMin = agr$Proportion - agr$CILow
-agr$YMax = agr$Proportion + agr$CIHigh
-
-dodge = position_dodge(.9)
-
-ggplot(agr, aes(x=EvidenceDirectness,y=Proportion)) +
-  geom_bar(stat="identity") +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),position=dodge,width=.25) +
-  facet_wrap(~Modal)
-ggsave("graphs/directness_categorical_means.pdf")
-
-r$BinaryStrength = as.factor(ifelse(r$Directness < .75, "weak","strong"))
-r$MixedEvidence = as.factor(paste(r$BinaryStrength,r$EvidenceTypeCategorical))
-summary(r[r$EvidenceTypeCategorical == "inferential",])
-r$FinalEvidence = as.character(r$MixedEvidence)
-r[r$EvidenceDirectnessCategorical == "direct",]$FinalEvidence = "direct"
-r[r$EvidenceTypeCategorical == "reportative",]$FinalEvidence = "report"
-r$FinalEvidence = as.factor(as.character(r$FinalEvidence))
-# do sth more reasonable than exclude these two categories
-r = droplevels(subset(r, ! FinalEvidence %in% c("weak perceptual","weak wishful")))
-
-t = as.data.frame(prop.table(table(r$item_type,r$FinalEvidence),mar=c(1)))
-head(t)
-t = droplevels(subset(t, Var1 %in% c("bare","must")))
-t$EType = factor(x=t$Var2, levels=c("direct","strong inferential","weak inferential","report"))
-ggplot(t, aes(x=Var1,y=Freq,fill=EType)) +
-  geom_bar(stat="identity",position="dodge",color="black") +
-  scale_fill_manual(values=c("#023858", "#1d91c0", "#7fcdbb", "#ffffcc"), name="Evidence type") +
-  xlab("Utterance") +
-  ylab("Proportion") +
-  theme(legend.position=c(.5,.8))
-ggsave("graphs/cuny_evidencedist.pdf",width=7)
-
-r$EvidenceType = as.factor(ifelse(r$EvidenceDirectnessCategorical == "direct","direct",ifelse(r$EvidenceTypeCategorical == "reportative","reportative","indirect")))
-un = unique(r[,c("Directness","item","EvidenceType")])
-un$StrengthBinNumObs = cut2(un$Directness,g=3)
-un$StrengthBinSize = cut(un$Directness,breaks=3)
-un$Strength = un$StrengthBinSize
-un$WordStrength = cut(un$Directness,breaks=3,labels=c("weak","medium","strong"))
-nrow(un)
+un = unique(r.combined[,c("Directness","item","response_evidence")])
+un$Strength = cut(un$Directness,breaks=3,labels=c("weak","medium","strong"))
+un$StrengthBin = cut(un$Directness,breaks=3)
 un
+nrow(un)
 
-ggplot(un, aes(x=Directness,fill=WordStrength)) +
-  geom_density(alpha=.5)
-
+library(fields)
 ggplot(un, aes(x=Directness, fill=Strength)) +
   geom_histogram() +
-  xlab("Strength")
-  #geom_density(aes(fill=WordStrength),alpha=.5)
-ggsave("graphs/strength_histogram_cuny.pdf",width=6,height=4)
+  scale_fill_manual(values=rev(designer.colors(n=3, col=c("#046C9A","#ABDDDE"))),name="Evidence\nstrength") +
+  #scale_fill_manual(values=wes_palette("Rushmore"),name="Evidence\nstrength") +  
+  xlab("Probability of q (evidence strength)") +
+  ylab("Number of cases") #+
+#  theme(legend.position="top")
+ggsave("graphs/strength_histogram.pdf",width=5.5,height=3.2)
 
-ggplot(un, aes(x=Directness,fill=EvidenceType)) +
-  geom_histogram()
 
-ggplot(un, aes(x=Directness)) +
-  geom_histogram()
-ggsave("graphs/evidencedirectness.pdf",width=6)
+### plot beliefs about q
+agr = aggregate(response ~ item_type + Experiment, FUN=mean, data=r.combined)
+agr$CILow = aggregate(response ~ item_type + Experiment, FUN=ci.low, data=r.combined)$response
+agr$CIHigh = aggregate(response ~ item_type + Experiment, FUN=ci.high, data=r.combined)$response
+agr$YMin = agr$response - agr$CILow
+agr$YMax = agr$response + agr$CIHigh
+head(agr)
+agr$Utterance = factor(x=as.character(agr$item_type),levels=c("wohl","vermutlich","muss","bare"))
+dodge = position_dodge(.9)
+
+ggplot(agr, aes(x=Utterance, y=response, fill=Experiment)) +
+  geom_bar(stat="identity",color="black",position=dodge) +
+  #  scale_fill_manual(values=designer.colors(n=3, col=c("#046C9A","#ABDDDE")),name="Belief",breaks=levels(agrr$Experiment),label=c("listener (Exp. 3a)", "speaker (Exp. 3b)")) +
+  scale_fill_manual(values=wes_palette("Moonrise2"),name="Belief",breaks=levels(agr$Experiment),label=c("listener\n(Exp. 3a)", "speaker\n(Exp. 3b)")) +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25,position=dodge) +
+  #facet_wrap(~ResponseType) +
+  scale_y_continuous("Probability of belief in q",breaks=seq(0,1,by=.1)) #+
+  #theme(legend.key.size = unit(1.2, "cm"))
+#  theme(legend.position="top",plot.margin=unit(c(-0.5,0,0,0),units="cm"))
+ggsave("graphs/mean_beliefs.pdf",width=5.5,height=3.5)
+#ggsave("pics/mean_beliefs_greg.pdf",width=7.8,height=3)
+
+
+## evidence
+r.combined$StrengthBinSize = cut(r.combined$Directness,breaks=3)
+r.combined$Strength = cut(r.combined$Directness,breaks=3,labels=c("weak","medium","strong"))
+
+head(r.combined)
+r.combined$Strength_strong = ifelse(r.combined$Strength == "strong",1,0)
+r.combined$Strength_medium = ifelse(r.combined$Strength == "medium",1,0)
+r.combined$Strength_weak = ifelse(r.combined$Strength == "weak",1,0)
+
+#library(dplyr)
+toplot = droplevels(r.combined) %>% 
+  select(Strength_strong,Strength_medium,Strength_weak,item_type) %>%
+  gather(strength, Proportion, -item_type)
+
+agr = aggregate(Proportion~strength+item_type,data=toplot,FUN="mean")
+agr$CILow = aggregate(Proportion~strength+item_type,data=toplot,FUN="ci.low")$Proportion
+agr$CIHigh = aggregate(Proportion~strength+item_type,data=toplot,FUN="ci.high")$Proportion
+agr$YMin = agr$Proportion - agr$CILow
+agr$YMax = agr$Proportion + agr$CIHigh
+t=agr
+t$StrengthBin = gsub("Strength_","",t$strength)
+head(t)
+dodge = position_dodge(.9) 
+
+t$Utterance = factor(x=as.character(t$item_type),levels=c("wohl","vermutlich","muss","bare"))
+t$Strength = factor(x=t$StrengthBin,levels=c("weak","medium","strong"))
+
+ggplot(t, aes(x=Utterance,y=Proportion,fill=Strength)) +
+  geom_bar(stat="identity",position=dodge,color="black") +
+  scale_fill_manual(values=rev(designer.colors(n=3, col=c("#046C9A","#ABDDDE"))),name="Evidence\nstrength") +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),position=dodge,width=.25) +
+  #scale_fill_manual(values=wes_palette("Royal1"),name="Evidence strength") + #Moonrise2, Darjeeling2, Moonrise3, Chevalier
+  ylab("Probability of evidence strength") +
+  #facet_wrap(~ResponseType) +
+  theme(plot.margin=unit(c(0,0,0,0),units="cm"),axis.title.y=element_text(size=14))  
+ggsave("graphs/evidence_dist.pdf")
+
+
